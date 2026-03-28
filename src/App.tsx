eb907ec14070521f97f4ae3ad41003e0551afc03
Abdelrhman Ahmed <mrblackvanta@gmail.com>
2026-03-28 08:50:01 +0200

    refactor: remove vite-tsconfig-paths dependency and update App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,31 @@ import { Test } from "components";
 
 export default function App() {
   return (
-      <div>
+    <div className="bg-yellow relative grid h-dvh place-content-center px-6.75">
+      <main>
         <Test />
-      </div>
+      </main>
+      <footer>
+        <footer className="text-preset-2 absolute bottom-4 w-full text-center font-light text-slate-900">
+          Challenge by{" "}
+          <a
+            href="https://www.frontendmentor.io?ref=challenge"
+            className="font-bold"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Frontend Mentor
+          </a>
+          . Coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/abdelrhman-vanta/"
+            className="font-bold"
+          >
+            Abdelrhman Abdelaal
+          </a>
+          .
+        </footer>
+      </footer>
+    </div>
   );
 }
