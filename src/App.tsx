ab4f297d440113e13dd5431e0bdfcd6947cbf41c
Abdelrhman Ahmed <mrblackvanta@gmail.com>
2026-03-28 09:41:05 +0200

    refactor: replace Test component with BlogPreviewCard and update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
-import { Test } from "components";
+import { BlogPreviewCard } from "components";
 
 export default function App() {
   return (
-    <div className="bg-yellow relative grid h-dvh place-content-center px-6.75">
+    <div className="bg-yellow relative grid h-dvh place-content-center px-6">
       <main>
-        <Test />
+        <BlogPreviewCard />
       </main>
-      <footer>
-        <footer className="text-preset-2 absolute bottom-4 w-full text-center font-light text-slate-900">
-          Challenge by{" "}
-          <a
-            href="https://www.frontendmentor.io?ref=challenge"
-            className="font-bold"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Frontend Mentor
-          </a>
-          . Coded by{" "}
-          <a
-            href="https://www.linkedin.com/in/abdelrhman-vanta/"
-            className="font-bold"
-          >
-            Abdelrhman Abdelaal
-          </a>
-          .
-        </footer>
+
+      <footer className="text-preset-2 absolute bottom-4 w-full text-center font-light text-gray-950">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          className="font-bold"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/abdelrhman-vanta/"
+          className="font-bold"
+        >
+          Abdelrhman Abdelaal
+        </a>
+        .
       </footer>
     </div>
   );
---
+++ src/components/BlogPreviewCard.tsx
@@ -0,0 +1,35 @@
+import { IllustrationArticleSVG } from "assets";
+import avatarImg from "assets/image-avatar.webp";
+
+export default function BlogPreviewCard() {
+  return (
+    <section className="grid max-w-96 place-items-center gap-6 rounded-[20px] border border-gray-950 bg-white p-6 text-gray-950 shadow-[8px_8px_0_0_#000]">
+      <figure className="overflow-hidden rounded-[10px]">
+        <IllustrationArticleSVG />
+      </figure>
+      <div className="space-y-3">
+        <h2 className="text-preset-3-bold bg-yellow w-fit rounded-sm px-3 py-1">
+          Learning
+        </h2>
+        <p className="text-preset-3">
+          Published <time dateTime="2023-12-21">21 Dec 2023</time>
+        </p>
+        <h1 className="text-preset-1">HTML & CSS foundations</h1>
+        <p className="text-preset-2 text-gray-500">
+          These languages are the backbone of every website, defining structure,
+          content, and presentation.
+        </p>
+      </div>
+      <figure className="flex items-center gap-3 justify-self-start">
+        <img
+          src={avatarImg}
+          alt="Greg Hooper"
+          className="size-8 overflow-hidden rounded-full object-cover"
+        />
+        <figcaption className="text-preset-3-bold text-sm">
+          Greg Hooper
+        </figcaption>
+      </figure>
+    </section>
+  );
+}
